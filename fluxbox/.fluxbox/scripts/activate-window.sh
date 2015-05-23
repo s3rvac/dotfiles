3eb033f777af918ef63634267bbfb8c108416139
Petr Zemek <s3rvac@gmail.com>
2015-05-23 18:20:42 +0200

    fluxbox: Add new keys.

---
+++ fluxbox/.fluxbox/scripts/activate-window.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Author: Petr Zemek <s3rvac@gmail.com>
+#
+# Activates a window of the given application ($1) on the current desktop. If
+# there is no such window, it runs the application.
+#
+
+APPLICATION="$1"
+DESKTOP=$(xdotool get_desktop)
+WINDOW=$(xdotool search --desktop "$DESKTOP" --class "$APPLICATION" | head -n1)
+if [ ! -z "$WINDOW" ]; then
+	# The window exists, so activate it.
+	xdotool windowactivate "$WINDOW"
+else
+	# The window does not exist, so run the application.
+	$APPLICATION
+fi
