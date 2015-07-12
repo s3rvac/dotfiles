bf10ebd23f6d43c15b947d8813e64d84dadcc347
Petr Zemek <s3rvac@gmail.com>
2015-07-12 17:01:38 +0200

    fluxbox: Fix the switch-screen-focus.sh script.
    The original version did not work properly when the target window had already focus.

--- fluxbox/.fluxbox/scripts/switch-screen-focus.sh
+++ fluxbox/.fluxbox/scripts/switch-screen-focus.sh
@@ -17,7 +17,7 @@
 # Should be either 0 or 1.
 SCREEN="$1"
 
-# Compute the position where we should move the mouse to switch focus.
+# Compute the position where we should move the mouse cursor to switch focus.
 if [ "$SCREEN" = "0" ]; then
 	SCREEN_MOUSE_X_POS="1600"
 else
@@ -25,7 +25,14 @@ else
 fi
 SCREEN_MOUSE_Y_POS="100"
 
-# Move the mouse to the target position and set focus to the window under it.
+# Move the mouse cursor to the target position.
 xdotool mousemove "$SCREEN_MOUSE_X_POS" "$SCREEN_MOUSE_Y_POS"
+
+# Get the window under the mouse cursor.
 WINDOW="$(xdotool getmouselocation --shell 2> /dev/null | grep 'WINDOW' | sed 's".*=\(.*\)"\1"')"
-xdotool windowfocus "$WINDOW"
+
+# Focus the window under the mouse cursor. However, do not set window focus if
+# the window already has focus (the window would loose focus instead).
+if [ "$(xdotool getwindowfocus)" != "$WINDOW" ]; then
+	xdotool windowfocus "$WINDOW"
+fi
