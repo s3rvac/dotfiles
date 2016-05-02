6d47e25e6a9bdad92685cebca927f62712c348a9
Petr Zemek <s3rvac@gmail.com>
2016-05-02 16:06:22 +0200

    fluxbox: Use a completely black background on both screens.

--- fluxbox/.fluxbox/scripts/set-background.sh
+++ fluxbox/.fluxbox/scripts/set-background.sh
@@ -2,21 +2,11 @@
 #
 # Author: Petr Zemek <s3rvac@gmail.com>
 #
-# Sets the background of the screen(s).
+# Sets the background of the screens.
 #
 
-# Find the screen resolution.
-# TODO When using dual heads, width is 3360 (or similar), i.e. the sum of the
-#      widths of both monitors.
-# LINE=`xrandr -q | grep Screen`
-# WIDTH=`echo ${LINE} | awk '{ print $8 }'`
-# HEIGHT=`echo ${LINE} | awk '{ print $10 }' | awk -F"," '{ print $1 }'`
-# RES="${WIDTH}x${HEIGHT}"
-
-# Choose a proper background image depending on the used resolution.
-# Note: I assume that the background file exists for the used resolution.
-SCREEN0_BG=~/.fluxbox/backgrounds/wallpaper.jpg
+# Use a completely black background on both screens.
+SCREEN0_BG=~/.fluxbox/backgrounds/black.png
 SCREEN1_BG=~/.fluxbox/backgrounds/black.png
 
-# Set the background.
 feh --image-bg black --bg-max "$SCREEN0_BG" "$SCREEN1_BG"
