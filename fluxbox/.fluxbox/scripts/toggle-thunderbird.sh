dcd023d91abd001a56b4b419bde4c367f6f9d913
Petr Zemek <s3rvac@gmail.com>
2017-03-18 19:34:49 +0100

    fluxbox: Make Ctrl-Alt-t toggle Thunderbird.

---
+++ fluxbox/.fluxbox/scripts/toggle-thunderbird.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# Author: Petr Zemek <s3rvac@gmail.com>
+#
+# Toggles Thunderbird:
+# - When it is not running, it starts it.
+# - When it is running and it is minimized, it brings it to the foreground.
+# - When it is running on the foreground, it is minized.
+#
+
+THUNDERBIRD_PID=$(pidof thunderbird)
+if [ -z "$THUNDERBIRD_PID" ]; then
+	# Thunderbird is not running -> start it.
+	thunderbird &
+	exit 0
+fi
+
+ACTIVE_WINDOW=$(xdotool getwindowfocus)
+THUNDERBIRD_WINDOW=$(xdotool search --name "Mozilla Thunderbird")
+if [ "$ACTIVE_WINDOW" != "$THUNDERBIRD_WINDOW" ]; then
+	# Thunderbird is minimized -> bring it to the foreground.
+	thunderbird -mail
+	exit 0
+fi
+
+# Minimize it.
+xdotool windowunmap "$THUNDERBIRD_WINDOW"
+exit 0
