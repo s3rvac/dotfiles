932053d8b6f887087d8ee8a239c3a04105124274
Petr Zemek <s3rvac@gmail.com>
2019-12-22 05:56:00 +0100

    fluxbox: Add a generic switching of screen focus.
    The original solution was fixed to a two-1920x1200-screen setup.

--- fluxbox/.fluxbox/scripts/switch-screen-focus.sh
+++ fluxbox/.fluxbox/scripts/switch-screen-focus.sh
@@ -2,31 +2,38 @@
 #
 # Author: Petr Zemek <s3rvac@gmail.com>
 #
-# Switches focus to the given screen.
+# Switches focus between screens ("left" or "right", depending on $1).
 #
-# We do this by moving mouse cursor to the given screen, getting a window
-# that's under the cursor, and focusing this window.
+# We do this by moving mouse cursor to the new screen, getting a window that's
+# under the cursor, and focusing this window.
 #
-# Note: Do not try to return the mouse cursor to its original position - focus
-#       switching then does not work!
-#
-# TODO The current (silly) implementation assumes that there are two screens, 0
-#      and 1, and that the first screen has 1920 pixels.
+# Notes:
+# - The current naive implementation assumes that windows on all screens are
+#   maximized, which suffices for me.
+# - Do not try to return the mouse cursor to its original position - focus
+#   switching then does not work!
 #
 
-# Should be either 0 or 1.
-SCREEN="$1"
+# Take current window's measurements.
+WIN_GEOMETRY=$(xdotool getactivewindow getwindowgeometry)
+WIN_W=$(grep 'Geometry' <<< "$WIN_GEOMETRY" | sed 's/  Geometry: \(.*\)x.*/\1/')
+WIN_H=$(grep 'Geometry' <<< "$WIN_GEOMETRY" | sed 's/  Geometry: .*x\(.*\)/\1/')
+WIN_X=$(grep 'Position' <<< "$WIN_GEOMETRY" | sed 's/  Position: \(.*\),.* (screen: .*)/\1/')
+WIN_Y=$(grep 'Position' <<< "$WIN_GEOMETRY" | sed 's/  Position: .*,\(.*\) (screen: .*)/\1/')
 
-# Compute the position where we should move the mouse cursor to switch focus.
-if [ "$SCREEN" = "0" ]; then
-	SCREEN_MOUSE_X_POS="1600"
+if [ "$1" = "left" ]; then
+	MOUSE_X=$((WIN_X - 10))
+	MOUSE_Y=100
+elif [ "$1" = "right" ]; then
+	MOUSE_X=$((WIN_X + WIN_W + 10))
+	MOUSE_Y=100
 else
-	SCREEN_MOUSE_X_POS="1950"
+	echo "error: not enough arguments or unsupported argument $1" >&2
+	exit 1
 fi
-SCREEN_MOUSE_Y_POS="100"
 
 # Move the mouse cursor to the target position, get the window under the
 # cursor, and activate the window.
-xdotool mousemove "$SCREEN_MOUSE_X_POS" "$SCREEN_MOUSE_Y_POS"
-WINDOW="$(xdotool getmouselocation --shell 2> /dev/null | grep 'WINDOW' | sed 's".*=\(.*\)"\1"')"
-xdotool windowactivate "$WINDOW"
+xdotool mousemove "$MOUSE_X" "$MOUSE_Y"
+WIN="$(xdotool getmouselocation --shell 2> /dev/null | grep 'WINDOW' | sed 's".*=\(.*\)"\1"')"
+xdotool windowactivate "$WIN"
