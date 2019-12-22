d281865e16c0f1a21ca18d1d93e17fe8558ddd3a
Petr Zemek <s3rvac@gmail.com>
2019-12-22 05:53:25 +0100

    fluxbox: Add mouse setup.

---
+++ fluxbox/.fluxbox/scripts/setup-mouse.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Author: Petr Zemek <s3rvac@gmail.com>
+#
+# Sets up the mouse.
+#
+
+MOUSE='Logitech G403 Prodigy Gaming Mouse'
+
+# Disable accelleration.
+xinput --set-prop "$MOUSE" 'libinput Accel Profile Enabled' 0, 1
+
+# Increase sensitivity.
+xinput --set-prop "$MOUSE" 'Coordinate Transformation Matrix' 1.600000, 0.000000, 0.000000, 0.000000, 1.600000, 0.000000, 0.000000, 0.000000, 1.000000
