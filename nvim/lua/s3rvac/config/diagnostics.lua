877a29af553d61ff6dba80c2a59231d67a9f385d
Petr Zemek <s3rvac@petrzemek.net>
2024-02-17 06:12:46 +0100

    nvim: Update my diagnostics configuration

--- nvim/lua/s3rvac/config/diagnostics.lua
+++ nvim/lua/s3rvac/config/diagnostics.lua
@@ -11,18 +11,19 @@ vim.diagnostic.config({
   update_in_insert = false,
   -- Settings for the floating window.
   float = {
-		border = "single",
-		-- Use a custom formatting function.
-		-- Message example:
-		--   "xyz" is not defined (Pyright) [reportUndefinedVariable]
-		suffix = "",
-		format = function(diagnostic)
-			return string.format(
-				"%s (%s) [%s]",
-				diagnostic.message,
-				diagnostic.source,
-				diagnostic.code or diagnostic.user_data.lsp.code
-			)
-		end,
-	},
+    border = "single",
+    -- Use a custom formatting function.
+    -- Message example:
+    --   "xyz" is not defined (Pyright) [reportUndefinedVariable]
+    suffix = "",
+    format = function(diagnostic)
+      return string.format(
+        "%s (%s) [%s]",
+        -- Remove trailing periods, e.g. "Lua Syntax Check." -> "Lua Syntax Check".
+        string.gsub(diagnostic.message, "%.$", ""),
+        string.gsub(diagnostic.source, "%.$", ""),
+        diagnostic.code or (diagnostic.user_data and diagnostic.user_data.lsp.code or "-")
+      )
+    end,
+  },
 })
