500de947bec164f36b095ab2d1c5500bc02f6b6a
Petr Zemek <s3rvac@petrzemek.net>
2024-02-17 06:10:50 +0100

    nvim: Load all Lua files in the config module automatically

--- nvim/lua/s3rvac/config/init.lua
+++ nvim/lua/s3rvac/config/init.lua
@@ -1,2 +1,7 @@
-require("s3rvac.config.colors")
-require("s3rvac.config.diagnostics")
+-- Load all Lua files in the present directory.
+local curr_dir = vim.fn.stdpath("config") .. "/lua/s3rvac/config"
+for _, file in ipairs(vim.fn.readdir(curr_dir, [[v:val =~ "\.lua$"]])) do
+  if file ~= "init.lua" then
+    require("s3rvac.config." .. file:gsub("%.lua$", ""))
+  end
+end
