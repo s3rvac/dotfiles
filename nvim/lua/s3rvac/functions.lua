d39bdfb115c320172e2ed4848ce9796498e02c22
Petr Zemek <s3rvac@petrzemek.net>
2024-02-17 06:10:01 +0100

    nvim: Add a new module: functions

---
+++ nvim/lua/s3rvac/functions.lua
@@ -0,0 +1,89 @@
+local M = {}
+
+-- Is the given file or program executable?
+function M.is_executable(file_or_prog)
+  return vim.fn.executable(file_or_prog) == 1
+end
+
+-- Unpacks the given table.
+function M.unpack(t)
+  -- Before Lua 5.2, unpack() was a global function. Since Lua 5.2, unpack() is
+  -- at table.unpack().
+  if table.unpack then
+    return table.unpack(t)
+  else
+    return unpack(t)
+  end
+end
+
+-- Returns a path to the given config file of a tool.
+function M.tool_config_path_for(config_file)
+  return vim.fn.stdpath("config") .. "/data/configs/" .. config_file
+end
+
+-- Returns a path to the given Mason-provided executable.
+function M.mason_bin_path_to(executable)
+  return vim.env.HOME .. "/.local/share/nvim/mason/bin/" .. executable
+end
+
+-- Returns true if the given Mason-provided executable exists, false otherwise.
+function M.mason_bin_exists(executable)
+  return vim.fn.executable(M.mason_bin_path_to(executable)) == 1
+end
+
+-- Returns mapping options with sensible defaults that can be overridden.
+function M.keymap_opts(t)
+  return {
+    noremap = t["noremap"] ~= nil and t["noremap"] or true,
+    silent = t["silent"] ~= nil and t["silent"] or true,
+    desc = t["desc"] ~= nil and t["desc"] or "",
+  }
+end
+
+-- Sets the selected indent style for the current buffer.
+local function set_indent_style(use_spaces, indent_length)
+  vim.opt_local.expandtab = use_spaces
+  vim.opt_local.tabstop = indent_length
+  vim.opt_local.softtabstop = indent_length
+  vim.opt_local.shiftwidth = indent_length
+end
+
+-- Sets the indent style for the current buffer to tabs.
+function M.set_indent_style_to_tabs()
+  set_indent_style(false, 4)
+end
+
+-- Sets the indent style for the current buffer to 4 spaces.
+function M.set_indent_style_to_4_spaces()
+  set_indent_style(true, 4)
+end
+
+-- Sets the indent style for the current buffer to 2 spaces.
+function M.set_indent_style_to_2_spaces()
+  set_indent_style(true, 2)
+end
+
+-- Interactively selects and prints a new indentation style for the current
+-- buffer.
+function M.select_indent_style()
+  vim.ui.select(
+    { "tabs", "4 spaces", "2 spaces" },
+    { prompt = "Select indent style:" },
+    function(choice)
+      if choice == "tabs" then
+        M.set_indent_style_to_tabs()
+        print("Indent: tabs")
+      elseif choice == "4 spaces" then
+        M.set_indent_style_to_4_spaces()
+        print("Indent: 4 spaces")
+      elseif choice == "2 spaces" then
+        M.set_indent_style_to_2_spaces()
+        print("Indent: 2 spaces")
+      end
+    end
+  )
+end
+
+--
+
+return M
