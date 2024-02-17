12ba0a23c7287893f262d7359bf6080020a22433
Petr Zemek <s3rvac@petrzemek.net>
2024-02-17 06:02:23 +0100

    nvim: Rename the local vimrc file to vimrc-local.vim

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,7 @@ local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
 vim.cmd.source(vimrc)
 
 -- My local vimrc.
-local vimrc_local = vim.fn.stdpath("config") .. "/vimrc.local.vim"
+local vimrc_local = vim.fn.stdpath("config") .. "/vimrc-local.vim"
 if vim.loop.fs_stat(vimrc_local) then
   vim.cmd.source(vimrc_local)
 end
