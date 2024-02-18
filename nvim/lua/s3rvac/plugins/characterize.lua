7ddda57d2a2623189095ef587f553e76bd4a410d
Petr Zemek <s3rvac@petrzemek.net>
2024-02-18 19:00:04 +0100

    nvim: Start using the vim-characterize plugin

---
+++ nvim/lua/s3rvac/plugins/characterize.lua
@@ -0,0 +1,6 @@
+-- Extends `ga` by additional information, such as Unicode character names.
+-- https://github.com/tpope/vim-characterize
+return {
+  "tpope/vim-characterize",
+  event = "VeryLazy",
+}
