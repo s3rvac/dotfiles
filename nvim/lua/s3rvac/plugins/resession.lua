97d87e87dfd538632c437b7b794551ccc02a2b93
Petr Zemek <s3rvac@petrzemek.net>
2024-02-18 18:57:11 +0100

    nvim: Update resession.nvim keymaps

--- nvim/lua/s3rvac/plugins/resession.lua
+++ nvim/lua/s3rvac/plugins/resession.lua
@@ -16,25 +16,26 @@ return {
     })
 
     -- Commands.
-    vim.api.nvim_create_user_command(
-      "SessSave",
-      function(opts)
-        resession.save(opts.fargs[1])
-      end,
-      {
-        nargs = "?",
-        desc = "Saves a session (optionally with the given name)",
-      }
+    vim.api.nvim_create_user_command("SessionSave", function(opts)
+      resession.save(opts.fargs[1])
+    end, {
+      nargs = "?",
+      desc = "Saves a session (optionally with the given name)",
+    })
+    vim.api.nvim_create_user_command("SessionLoad", function(opts)
+      resession.load(opts.fargs[1])
+    end, {
+      nargs = "?",
+      desc = "Loads a session (optionally with the given name)",
+    })
+
+    -- Mappings.
+    local fns = require("s3rvac.functions")
+    vim.keymap.set(
+      "n",
+      "<Leader>sl",
+      resession.load,
+      fns.keymap_opts({ desc = "Load session (interactively)" })
     )
-    vim.api.nvim_create_user_command(
-      "SessLoad",
-      function(opts)
-        resession.load(opts.fargs[1])
-      end,
-      {
-        nargs = "?",
-        desc = "Loads a session (optionally with the given name)"
-      }
-    )
-  end
+  end,
 }
