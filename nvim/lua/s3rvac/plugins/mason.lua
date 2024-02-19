5ec7da0a4886a6e7fc4dca2e3144cb7841a6c3cc
Petr Zemek <s3rvac@petrzemek.net>
2024-02-19 15:23:44 +0100

    nvim: Update my Mason configuration

--- nvim/lua/s3rvac/plugins/mason.lua
+++ nvim/lua/s3rvac/plugins/mason.lua
@@ -7,12 +7,13 @@ return {
   event = "VeryLazy",
   dependencies = {
     { "williamboman/mason-lspconfig.nvim", tag = "v1.26.0" },
+    { "WhoIsSethDaniel/mason-tool-installer.nvim" },
   },
   config = function()
     local mason = require("mason")
     local mason_lspconfig = require("mason-lspconfig")
 
-    -- Enable and configure Mason.
+    -- Enable and configure mason.
     mason.setup({
       ui = {
         icons = {
@@ -23,12 +24,36 @@ return {
       },
     })
 
-    -- Enable and configure the installation of LSP servers.
-    mason_lspconfig.setup({
-      -- Disable automatic installation of servers. Reason: I prefer to install
-      -- those manually as I use Neovim on many machines and not all of those
-      -- are capable of running all servers.
-      automatic_installation = false,
+    -- Enable and configure mason-lspconfig.
+    mason_lspconfig.setup()
+
+    -- LSP servers, formatters, and linters to install via :MasonToolsInstall.
+    require("mason-tool-installer").setup({
+      ensure_installed = {
+        -- LSP servers.
+        "bash-language-server",
+        "dockerfile-language-server",
+        "lua_ls",
+        "pyright",
+        "terraformls",
+        "yaml-language-server",
+
+        -- Formatters.
+        "jq",
+        "ruff",
+        "shfmt",
+        "stylua",
+        "yq",
+
+        -- Linters.
+        "luacheck",
+        "ruff",
+        "shellcheck",
+        "tflint",
+      },
+
+      -- Do not run :MasonToolsInstall on startup (I prefer to run it manually).
+      run_on_start = false,
     })
   end,
 }
