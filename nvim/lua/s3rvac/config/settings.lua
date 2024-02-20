1a0b1663da197028a5e8d70cb43dc7c6b495587b
Petr Zemek <s3rvac@petrzemek.net>
2024-02-20 15:44:09 +0100

    nvim: Start using 'exrc' for managing project-local configuration

--- nvim/lua/s3rvac/config/settings.lua
+++ nvim/lua/s3rvac/config/settings.lua
@@ -1,3 +1,8 @@
+-- Enable project-local configuration by automatically executing .nvim.lua,
+-- .nvimrc, and .exrc files in the current directory if the file is in the
+-- trust list.
+vim.opt.exrc = true
+
 -- Force Neovim to always use the system version of Python. Otherwise, when
 -- opening Neovim from within a virtual environment (e.g. Python's venv),
 -- Python plugins in Neovim would complain that `pynvim` is not installed.
