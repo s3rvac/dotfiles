7a779886ce1358c2f2dbb2614aef67732dc7c5d0
Petr Zemek <s3rvac@petrzemek.net>
2024-02-26 14:29:52 +0100

    nvim: Add a new plugin: tpope/vim-speeddating

---
+++ nvim/lua/s3rvac/plugins/speedating.lua
@@ -0,0 +1,6 @@
+-- Use CTRL-A/CTRL-X to increment dates, times, and more.
+-- https://github.com/tpope/vim-speeddating
+return {
+  "tpope/vim-speeddating",
+  commit = "5a36fd29df63ea3f65562bd2bb837be48a5ec90b", -- 2022-10-22
+}
