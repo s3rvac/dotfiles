f7553f61357dfff726f8c02773ed69107398810c
Petr Zemek <s3rvac@petrzemek.net>
2024-02-27 16:09:02 +0100

    nvim: Fix the setting of an option for the unicode.vim plugin

--- nvim/lua/s3rvac/plugins/unicode.lua
+++ nvim/lua/s3rvac/plugins/unicode.lua
@@ -5,7 +5,7 @@ return {
   commit = "bc20d0fb3331a7b41708388c56bb8221c2104da7", -- 2023-09-20
   config = function()
     -- Use custom mappings.
-    vim.g["g:Unicode_no_default_mappings"] = true
+    vim.g.Unicode_no_default_mappings = true
 
     -- Mappings.
     vim.keymap.set("i", "<C-x><C-u>", "<Plug>(UnicodeComplete)", {
