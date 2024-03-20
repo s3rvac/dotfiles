fef90a1dc58f0ead99e760ee1399ddf9984a3ffd
Petr Zemek <s3rvac@petrzemek.net>
2024-03-20 15:52:58 +0100

    nvim: Update my colorscheme

--- nvim/lua/s3rvac/colorscheme/theme.lua
+++ nvim/lua/s3rvac/colorscheme/theme.lua
@@ -86,7 +86,7 @@ function theme.setup()
     SpellRare = { sp = "#ff00ff", undercurl = true },
     Statement = { fg = "#ffff60", bg = p.none, bold = true },
     StatusLine = { fg = p.white, bg = p.black, bold = true },
-    StatusLineNC = { fg = p.black, bg = "gray70" },
+    StatusLineNC = { fg = p.white, bg = p.black, bold = false },
     StatusLineTerm = { link = "StatusLine" },
     StatusLineTermNC = { link = "StatusLineNC" },
     StorageClass = { link = "Type" },
