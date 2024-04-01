198c00bb50c0f693578f199ae7e4682055e8fa59
Petr Zemek <s3rvac@petrzemek.net>
2024-04-01 07:10:09 +0200

    nvim: Use my fork of the vim-tag-closer plugin
    My fork of the plugin uses insert instead of append to insert the closing tag.

--- nvim/lua/s3rvac/plugins/close-tag.lua
+++ nvim/lua/s3rvac/plugins/close-tag.lua
@@ -1,8 +1,10 @@
 -- Closing HTML/XML tags on demand.
 -- https://github.com/airblade/vim-tag-closer
 return {
-  "airblade/vim-tag-closer",
-  commit = "d812d765fef3caf5a4097dadf7c565e0a01a2f36", -- 2024-03-05
+  -- Use my fork of the plugin that uses insert instead of append to insert the
+  -- closing tag.
+  "s3rvac/vim-tag-closer",
+  branch = "insert-instead-of-append",
   ft = { "html", "xhtml", "xml" },
   init = function()
     -- Disable the default keymaps and use my own keymap.
