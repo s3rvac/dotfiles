d69a858d163ea69ec79a1e585a1fc26ec2f65f2f
Petr Zemek <s3rvac@petrzemek.net>
2024-04-30 16:58:43 +0200

    nvim: Change the behavior of the custom keymaps for joining lines
    Do not preserve the cursor position when joining lines as I have found that I
    like the original behavior more.

--- nvim/lua/s3rvac/config/keymaps.lua
+++ nvim/lua/s3rvac/config/keymaps.lua
@@ -286,23 +286,15 @@ if !has('gui_running')
 endif
 
 " Use a custom keymap for joining lines as I use J to go to the previous tab.
-" Also, when joining lines, keep the cursor as-is instead of moving it to the
-" end of the line.
-noremap <Leader>jj mzJ`z
+noremap <Leader>jj J
 
 " Join lines without producing any spaces. It works like gJ, but does not keep
 " the indentation whitespace.
-" Also, when joining lines, keep the cursor as-is instead of moving it to the
-" end of the line.
 function! s:JoinWithoutSpaces()
-  " Store the current cursor position and join the lines.
-  normal! mzgJ
-  " Remove any whitespace.
+  normal! gJ
   if matchstr(getline('.'), '\%' . col('.') . 'c.') =~ '\s'
     normal! dw
   endif
-  " Go back to the stored cursor position.
-  normal! `z
 endfunction
 noremap <silent> <Leader>JJ :call <SID>JoinWithoutSpaces()<CR>
 
