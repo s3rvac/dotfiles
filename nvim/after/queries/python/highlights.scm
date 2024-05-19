08d971e36bfb1acfda6fba74e4394498ee2d77a7
Petr Zemek <s3rvac@petrzemek.net>
2024-05-19 15:26:54 +0200

    nvim: Improve Python highlighting
    Highlight also decorators of the form `@x.y.z` and `@x.y.z()` (i.e. not just
    `x.y` but also `x.y.z`).

--- nvim/after/queries/python/highlights.scm
+++ nvim/after/queries/python/highlights.scm
@@ -3,19 +3,61 @@
 ; Recognize class names in definitions as type definitions.
 (class_definition name: (identifier) @type.definition)
 
-; Recognize decorators of the form `@object.attribute` (e.g.
-; `@dataclass.dataclass`). Without this, `object` is not highlighted.
+; Recognize decorators of the form `@x.z.y` (e.g. `@dataclass.dataclass`) and
+; make them highlighted
 (decorator
   (attribute
     object: (identifier) @attribute))
+(decorator
+  (attribute
+   object: (attribute
+     object: (identifier) @attribute
+     attribute: (identifier) @attribute)
+   attribute: (identifier)))
+(decorator
+  (attribute
+   object: (attribute
+     object: (identifier) @attribute
+     attribute: (identifier) @attribute)
+   attribute: (identifier)))
+(decorator
+  (attribute
+    object: (attribute
+      object: (attribute
+        object: (identifier) @attribute
+        attribute: (identifier) @attribute)
+      attribute: (identifier) @attribute)
+    attribute: (identifier)))
 
-; Recognize decorators of the form `@object.attribute()` (e.g.
-; `@dataclass.dataclass(frozen=True)`). Without this, `object` is not
-; highlighted.
+; Recognize decorators of the form `@x.z.y()` (e.g.
+; `@dataclass.dataclass(frozen=True)`) and make them highlighted
 (decorator
   (call
     (attribute
       object: (identifier) @attribute)))
+(decorator
+  (call
+    (attribute
+     object: (attribute
+       object: (identifier) @attribute
+       attribute: (identifier) @attribute)
+     attribute: (identifier))))
+(decorator
+  (call
+    (attribute
+     object: (attribute
+       object: (identifier) @attribute
+       attribute: (identifier) @attribute)
+     attribute: (identifier))))
+(decorator
+  (call
+    (attribute
+      object: (attribute
+        object: (attribute
+          object: (identifier) @attribute
+          attribute: (identifier) @attribute)
+        attribute: (identifier) @attribute)
+      attribute: (identifier))))
 
 ; Recognize __init__/__new__ as methods. Without this, they are recognized as
 ; @constructor (similarly to class instantiations), which makes them
