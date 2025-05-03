b99890bdc57e321781f75e0b645c6882f9fcf437
Petr Zemek <s3rvac@petrzemek.net>
2025-05-03 08:43:49 +0200

    fluxbox: Add a key bind for clearing notifications
    This allows me to clear all notifications sent by notify-send via keyboard.

---
+++ fluxbox/.fluxbox/scripts/clear-notifications.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Author: Petr Zemek <s3rvac@petrzemek.net>
+#
+# Clears all notifications sent by notify-send.
+#
+
+for window_id in $(xdotool search --class xfce4-notifyd); do
+   xdotool windowkill "$window_id"
+done
