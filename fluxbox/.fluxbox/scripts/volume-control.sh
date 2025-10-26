45a82def6a8b365cb47d584d4ad614d4573c38e1
Petr Zemek <s3rvac@petrzemek.net>
2025-10-26 12:05:51 +0100

    fluxbox: Switch from volumeicon to custom volume control script
    Reason: volumeicon is no longer maintained (the repo has been archived) and
    does no longer work on my system after an update.

---
+++ fluxbox/.fluxbox/scripts/volume-control.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#
+# Author: Petr Zemek <s3rvac@petrzemek.net>
+#
+# Volume control.
+#
+# Requirements:
+#   - pactl (from PulseAudio)
+#   - osd_cat (from xosd-bin)
+#
+
+get_current_volume() {
+	pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\d+%' | head -n1 | sed 's/%//'
+}
+
+get_mute_unmute_status() {
+	pactl get-sink-mute @DEFAULT_SINK@ | grep -Po 'yes|no' | sed 's/yes/Muted/' | sed 's/no/Unmuted/'
+}
+
+show_volume_status() {
+	status="${1}"
+	echo "Volume: $status"
+
+	# Kill any existing osd_cat instances to avoid overlapping messages.
+	pkill osd_cat
+	echo "${status}" | osd_cat --delay=1 --age=0 --align=right --pos=bottom --offset=-100 --color=white --font=lucidasans-24 &
+}
+
+if [ "$1" = "up" ]; then
+	if [ "$(get_current_volume)" -le 95 ]; then
+		pactl set-sink-volume @DEFAULT_SINK@ +5%
+	else
+		pactl set-sink-volume @DEFAULT_SINK@ 100%
+	fi
+	show_volume_status "$(get_current_volume)%"
+elif [ "$1" = "down" ]; then
+	if [ "$(get_current_volume)" -ge 5 ]; then
+		pactl set-sink-volume @DEFAULT_SINK@ -5%
+	else
+		pactl set-sink-volume @DEFAULT_SINK@ 0%
+	fi
+	show_volume_status "$(get_current_volume)%"
+elif [ "$1" = "toggle" ]; then
+	pactl set-sink-mute @DEFAULT_SINK@ toggle
+	show_volume_status "$(get_mute_unmute_status)"
+elif [ "$1" = "status" ]; then
+	show_volume_status "$(get_current_volume)%"
+else
+	echo "Usage: $0 {up|down|toggle|status}" >&2
+	exit 1
+fi
