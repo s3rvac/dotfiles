80ecf22fd3053d060502310aaf5b8845d62f9a58
Petr Zemek <s3rvac@petrzemek.net>
2025-11-01 09:54:30 +0100

    fluxbox: Fix xsecurelock compatibility with xcompmgr

--- fluxbox/.fluxbox/scripts/lock-screen.sh
+++ fluxbox/.fluxbox/scripts/lock-screen.sh
@@ -9,4 +9,8 @@
 sleep 0.3; xset dpms force off
 
 # Lock the screen.
-XSECURELOCK_SAVER=saver_blank XSECURELOCK_AUTH=auth_pam_x11 xsecurelock
+#
+# XSECURELOCK_NO_COMPOSITE=1 is there to fix compatibility issues of xsecurelock
+# with the xcompmgr compositor ("INCOMPATIBLE COMPOSITOR, PLEASE FIX!").
+# https://github.com/google/xsecurelock?tab=readme-ov-file#known-compatibility-issues
+XSECURELOCK_SAVER=saver_blank XSECURELOCK_AUTH=auth_pam_x11 XSECURELOCK_NO_COMPOSITE=1 xsecurelock
