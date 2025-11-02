a5310d92bb798380820aca5ff4f75a98c9680aa4
Petr Zemek <s3rvac@petrzemek.net>
2025-11-02 15:48:11 +0100

    nvim: Update several dependencies to their latest versions

--- nvim/lua/s3rvac/plugins/argwrap.lua
+++ nvim/lua/s3rvac/plugins/argwrap.lua
@@ -1,7 +1,7 @@
 -- Wrap and unwrap function arguments, lists, and dictionaries.
 -- https://git.foosoft.net/alex/vim-argwrap
 return {
-  url = "https://git.foosoft.net/alex/vim-argwrap",
-  tag = "24.1.15.0",
+  url = "https://github.com/FooSoft/vim-argwrap", -- 2025-10-28: https://git.foosoft.net/alex/vim-argwrap does not work
+  commit = "f3e26a5ad249d09467804b92e760d08b1cc457a1",
   event = "VeryLazy",
 }
--- nvim/lua/s3rvac/plugins/github-copilot.lua
+++ nvim/lua/s3rvac/plugins/github-copilot.lua
@@ -2,7 +2,7 @@
 -- https://github.com/github/copilot.vim
 return {
   "github/copilot.vim",
-  tag = "v1.48.0", -- 2025-05-05
+  tag = "v1.56.0", -- 2025-10-01
   config = function()
     -- Add an option to enable or disable copilot, e.g. via exrc.
     -- Note: This is my own variable, not the one provided by the plugin.
