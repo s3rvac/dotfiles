f95108936c5394bb331313cf25b46b95b13a1863
Petr Zemek <s3rvac@petrzemek.net>
2026-08-04 17:56:50 +0200

    nvim: Add an explicit nvim-lua/plenary dependency

---
+++ nvim/lua/s3rvac/plugins/plenary.lua
@@ -0,0 +1,10 @@
+-- Common Neovim functions.
+-- https://github.com/nvim-lua/plenary.nvim
+--
+-- Currently used as a dependency of CopilotChat.nvim (I have it configured as
+-- a standalone plugin so that I can have the same lazy.json config across
+-- machines).
+return {
+  "nvim-lua/plenary.nvim",
+  commit = "74b06c6c75e4eeb3108ec01852001636d85a932b", -- 2026-04-10
+}
